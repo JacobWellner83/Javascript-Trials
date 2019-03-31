63cd053c48a797170e18bbec95cb23c133926866
JacobWellner83 <jcwellner83@gmail>
2019-03-31 10:24:24 -0500

    reformatted javascript file

--- Trials.js
+++ Trials.js
@@ -1,11 +1,12 @@
 //Notes:
-//*Don't forget to return the result.
+//*Don't forget to return or console.log the result.
 //*All tests contain valid numbers.
 
 //1. Create a function that takes two numbers as arguments and return their sum.
 
 
 //2. Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
 
 //3.Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
@@ -32,6 +33,7 @@
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
 
 //4. Create an if-else statement that will randomly log "Marco" half of the time and "Polo" the other half.  
 const random = Math.random();
